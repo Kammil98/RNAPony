75e1bbbe1b64beb76b89dee549790ea5c262e435
Kammil98 <kamil.luwanski@gmail.com>
2020-06-19 08:24:50 +0200

    tried to add lombok and Add Homology module

--- new One/RNAPony/Cse/src/main/java/module-info.java
+++ new One/RNAPony/Cse/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module Cse {
     requires Tokenizer;
     requires java.logging;
+    //requires lombok;
     //requires transitive lombok;
     opens cse;
     exports cse;
---
+++ new One/RNAPony/Homology/src/main/java/Homology.java
@@ -0,0 +1,5 @@
+public class Homology {
+    public void bpSet(StringBuilder seq, StringBuilder dot){
+        int l = dot.length();
+    }
+}
