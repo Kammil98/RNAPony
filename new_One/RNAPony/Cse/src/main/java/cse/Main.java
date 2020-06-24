84c08e17636550ed28e75cb7725f99870a767427
Kammil98 <kamil.luwanski@gmail.com>
2020-06-24 20:00:12 +0200

    Uncomment main function

--- new_One/RNAPony/Cse/src/main/java/cse/Main.java
+++ new_One/RNAPony/Cse/src/main/java/cse/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args){
         Hairpin hairpin = new Hairpin("hairpin.dot", "cse.txt", 0);
         hairpin.findSequences();
-        /*Loop loop = new Loop("bulge.dot", "cse.txt", 0, false);
-        loop.findSequences();*/
+        Loop loop = new Loop("ur4_L1.dot", "cse.txt", 0, false);
+        loop.findSequences();
         //Loop loop = new Loop("ur15_spinka.dot", "cse.txt", 0, false);
         //loop.setSaveToFile(true);
         //loop.changeLogFile(Path.of("plik.txt"));
         //loop.findSequences();
-        //Homology homology = new Homology();
-        //homology.compute("ur4_L1_0.txt");
+        Homology homology = new Homology();
+        homology.compute("ur4_L1_0.txt");
     }
 }
